892fdce113d0a426aa582f9def388525690221ca
Massiel50 <massielescano@yahoo.com>
2020-02-18 12:32:12 -0500

    added searchfrom component to home page

--- client/src/components/Pages/home.js
+++ client/src/components/Pages/home.js
@@ -1,8 +1,12 @@
 import React from "react";
+import SearchForm from "./searchForm";
 
 function Home(props) {
   return (
       <div>
+          <div>
+              <SearchForm/>
+          </div>
 
       <div>
         <a href="#home" onClick={() => props.handlePageChange("Home")} className="nav-link">
