5c89b1ffa65c507556e12213c4a4b72d92dfe715
Massiel50 <massielescano@yahoo.com>
2020-02-18 11:04:04 -0500

    starting paths, added required imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React from "react";
+import Map from "./components/API/map"
 import "./App.css";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h2>Welcome to React</h2>
       </div>
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
+      <div className="loadMap">
+        <Map />
+      </div>
     </div>
   );
 }
--- client/src/components/API/map.js
+++ client/src/components/API/map.js
@@ -1,46 +1,30 @@
-import React from "react";
+import React, {Component} from "react";
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 // let axios = require("axios");
 let APIKEY = "AIzaSyDJZVKAh9W4Op0a4i7YnnQ8L4TG7IbdUu4";
 
-// // will make call to api
-//  let request =
-// "https://maps.googleapis.com/maps/api/js?key=" + APIKEY + "&callback=" + initMap
+// will make call to api
+export default {
+accessAPI: function(){
+    axios.get(request)
+ .then(function(res) {
+   console.log(res);
+   // embed onto the page map itself
+   <iframe
+   width="450"
+   height="250"
+   frameborder="0" style="border:0"
+   src="https://www.google.com/maps/embed/v1/search?key={APIKEY}&q=record+stores+in+Seattle" allowfullscreen>
+ </iframe>
+ })
+}
 
-// axios.get(request)
-// .then(function(res) {
-//   console.log(res);
-// })
-
-function MapImage() {
-    return (
-        <div className="loadMap">
-{/* embed onto the page map itself */}
-<div>
-    map:
-
-    width="450"
-    height="250"
-    frameborder="0" style="border:0"
-    src="https://www.google.com/maps/embed/v1/search?key={APIKEY}&q=record+stores+in+Seattle" allowfullscreen>
-
-</div>
-        </div>
-
-    );
-  }
-  
-  
-  export default MapImage;
+}
+ 
     
 
 
-    // embed onto the page map itself
-//     <iframe
-//     width="450"
-//     height="250"
-//     frameborder="0" style="border:0"
-//     src="https://www.google.com/maps/embed/v1/search?key=YOUR_API_KEY&q=record+stores+in+Seattle" allowfullscreen>
-//   </iframe>
+    
 
 
 
