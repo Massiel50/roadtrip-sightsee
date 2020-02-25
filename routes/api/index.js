51a7cca821b95edc1eded245e373306fa751e201
Massiel50 <massielescano@yahoo.com>
2020-02-25 15:04:37 -0500

    adding bcrypt

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const loginController = require("../../controller/logIn");
 
-const placeRoutes = require("./places");
+// const placeRoutes = require("./places");
 
 // api routes for user auth
 router.route("/signup").post(loginController.signUp);
@@ -10,6 +10,6 @@ router.route("/signin").post(loginController.signIn);
 router.route("/verify").post(loginController.verify);
 router.route("/logout").post(loginController.logout);
 
-router.use("/places", placeRoutes);
+// router.use("/places", placeRoutes);
 
 module.exports = router;
